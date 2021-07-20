95fa2322c2847b6f3fd75e6905c545e6f4e8be3e
Mitchell Crane <mitchellhcrane@gmail.com>
2021-07-20 11:36:54 -0700

    else statement test

--- server.js
+++ server.js
@@ -36,27 +36,29 @@ app.post("/api/student", (req, res) => {
       author: "logan",
       type: "manual",
     });
-
     res.status(200).send(studentList);
-  } else if (name === "") {
-    // add a rollbar error here
-    rollbar.error("no name given");
-
-    res.status(400).send({ error: "no name was provided" });
-  } else if (name === "Mitch" || name === "Mitch") {
-    rollbar.warning("You can not add Mitch to the list");
-    res.status(400).send("You cannot add Mitch to the list");
-  } else if (name === null) {
-    rollbar.error("you have to add something to the list");
-    res.status(400).send({ error: "you have to add a name to the list" });
-  } else if (name === "jessica") {
-    rollbar.critical("you can not add jessica to the list");
-    res.status(400).send({ error: "you can not add jessica to the list" });
   } else {
-    rollbar.error("student already exists");
-    alert("student already exists");
-    res.status(400).send({ error: "that student already exists" });
+    rollbar.warning("Name already added");
+    res.status(400).send("Name already added");
   }
+  // } else if (name === "") {
+  //   // add a rollbar error here
+  //   rollbar.error("no name given");
+  //   res.status(400).send({ error: "no name was provided" });
+  // } else if (name === "Mitch" || name === "Mitch") {
+  //   rollbar.warning("You can not add Mitch to the list");
+  //   res.status(400).send("You cannot add Mitch to the list");
+  // } else if (name === null) {
+  //   rollbar.error("you have to add something to the list");
+  //   res.status(400).send({ error: "you have to add a name to the list" });
+  // } else if (name === "jessica") {
+  //   rollbar.critical("you can not add jessica to the list");
+  //   res.status(400).send({ error: "you can not add jessica to the list" });
+  // } else {
+  //   rollbar.error("student already exists");
+  //   alert("student already exists");
+  //   res.status(400).send({ error: "that student already exists" });
+  // }
 });
 
 const port = process.env.PORT || 4545;
